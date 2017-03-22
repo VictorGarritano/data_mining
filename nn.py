8b32b116abbd7fec07e549d65c48692355f6a4ec
VictorGarritano <victorg@ufrj.br>
2017-03-22 16:56:02 -0300

    XOR neural network
    Implementação de rede neural simples, com 1 hidden layer, que simula um XOR

--- nn.py
+++ nn.py
@@ -4,7 +4,6 @@ x = np.array([[0,0,1],[0,1,1],[1,0,1],[1,1,1]])
 y = np.array([[0],[1],[1],[0]])
 w1 = np.random.normal(size=(3,5))
 w2 = np.random.normal(size=(5,1))
-lr = 1e-2
 
 def sigmoid(x):
 	return 1.0 / (1.0 + np.exp(-x))
@@ -15,17 +14,21 @@ def sigmoid_prime(x):
 for epoch in range(60000):
 	h = x.dot(w1)
 	h_activated = sigmoid(h)
-	y_hat = h_activated.dot(w2)
+	y_ball = h_activated.dot(w2)
+	y_hat = sigmoid(y_ball)
 
-	loss = np.mean(0.5 * np.square(y_hat - y))
-	if epoch % 5000 == 0:
+	loss = np.mean(0.5 * np.square(y - y_hat))
+	if epoch % 1000 == 0:
 		print ('epoch: {}	loss: {}'.format(str(epoch), str(loss)))
 
-	grad_y_hat = (y_hat - y)
-	grad_w2 = h_activated.T.dot(grad_y_hat)
-	grad_w1 = x.T.dot(sigmoid_prime(h) * (np.dot(grad_y_hat, w2.T)))
+	l2_loss = -(y - y_hat)
+	l2_delta = l2_loss * sigmoid_prime(y_ball)
+	grad_w2 = h_activated.T.dot(l2_delta)
+	l1_error = l2_delta.dot(w2.T)
+	l1_delta = l1_error * sigmoid_prime(h)
+	grad_w1 = x.T.dot(l1_delta)
 
-	w1 -= lr * grad_w1
-	w2 -= lr * grad_w2
+	w1 -= grad_w1
+	w2 -= grad_w2
 
 print y_hat
